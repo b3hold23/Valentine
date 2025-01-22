741e052542770d392b0285903c644bb41a91aa56
unknown <rosa.angel.daniel@gmail.com>
2025-01-21 21:35:58 -0700

    small corrections

--- src/pages/Date.jsx
+++ src/pages/Date.jsx
@@ -1,10 +1,10 @@
 
 const Date = () => {
     return (
-        <img src="https://media.giphy.com/media/3o7TKz9bX9YKuK9KJ6/giphy.gif" alt="Date Idea" />;
-        <h1>Date Idea</h1>
-    );
-};
+        <img src="https://media.giphy.com/media/3o7TKz9bX9YKuK9KJ6/giphy.gif" alt="Date Idea" />
+        
+    )
+}
 
 export default Date;
 
