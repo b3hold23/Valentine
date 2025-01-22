ae3ba3bbbab23a8e00b07e91d8c1fba58f524f82
unknown <rosa.angel.daniel@gmail.com>
2025-01-21 22:01:35 -0700

    Added gif to date page

--- src/pages/Date.jsx
+++ src/pages/Date.jsx
@@ -1,8 +1,7 @@
 
 const Date = () => {
     return (
-        <img src="https://media.giphy.com/media/3o7TKz9bX9YKuK9KJ6/giphy.gif" alt="Date Idea" />
-        
+        <img src='https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExaTlnYXVsZWV6dmY0NDQ4MTMwYW43Y2JueGN1cjQzeXdpMmttOWFreiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/d3MM4vAejYomlzGg/giphy.gif'></img>
     )
 }
 
