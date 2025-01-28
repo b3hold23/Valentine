19194edf45888ae9acaa74994400afe8d2be6f5d
unknown <rosa.angel.daniel@gmail.com>
2025-01-28 00:55:26 -0700

    going to add input for form

--- src/components/DateForm.jsx
+++ src/components/DateForm.jsx
@@ -1,12 +1,57 @@
+import { useState } from "react";
+import { DatePicker } from "react-date-picker";
 
-const DateForm = () => {
+import '../Calendar.css';
+import 'react-date-picker/dist/DatePicker.css';
+// import 'react-calendar/dist/Calendar.css';
+
+const LocateDate = () => {
+    return (
+        <div>
+            <h2>Where would you like to go?</h2>
+            <input type="text" placeholder="Location" />
+        </div>
+    );
+}
+const DateDay = () => {
+    const [date, setDate] = useState(new Date());
+    
+    return (
+        <>
+        <header>
+            <h2>Plan your date:</h2>
+        </header>
+        <DatePicker
+        calendarAriaLabel="Toggle calendar"
+        dayAriaLabel="Day"
+        monthAriaLabel="Month"
+        nativeInputAriaLabel="Date"
+        onChange={setDate}
+        value={date}
+        yearAriaLabel="Year"
+        locale="en-EN"
+        onCalendarClose={() => console.log(`Calendar closed, date selected: ${date.toDateString()}`)}
+        />
+        </>
+    );
+    
+}
+const DateNight = () => {
+    const [dateBtn, startDateBtn] = useState(false);
+
     const HandleClick = () => {
-        alert('You clicked me!');
-    }
+        startDateBtn(!dateBtn);
+    };
 
     return (
-        <button onClick={HandleClick}>Click me</button>
+        <div>
+         <button onClick={HandleClick}>
+            {dateBtn ? "Close" : "❤️ Click here to start planning your date ❤️"}
+         </button>
+         {dateBtn && <DateDay />}
+        </div>
     );
 }
 
-export default DateForm;
+export default DateNight;
+
--- src/pages/Date.jsx
+++ src/pages/Date.jsx
@@ -1,44 +1,21 @@
-import { useState } from "react";
-import { DatePicker } from "react-date-picker";
 
 
-import DateForm from "../components/DateForm";
+
+import DateNight from "../components/DateForm";
 import 'react-date-picker/dist/DatePicker.css';
 import '../Calendar.css';
-
-const DateDay = () => {
-    const [date, setDate] = useState(new Date());
-
-    return (
-        <>
-        <header>
-            You selected
-        </header>
-        <DatePicker
-        calendarAriaLabel="Toggle calendar"
-        dayAriaLabel="Day"
-        monthAriaLabel="Month"
-        nativeInputAriaLabel="Date"
-        onChange={setDate}
-        value={date}
-        yearAriaLabel="Year"
-        />
-        </>
-    );
-}
-
-const DateNight = () => {
+const DatePlan = () => {
     return (
         <>
         <img src='https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExaTlnYXVsZWV6dmY0NDQ4MTMwYW43Y2JueGN1cjQzeXdpMmttOWFreiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/d3MM4vAejYomlzGg/giphy.gif'></img>
 
         <h1>Initial wording test</h1>
-        <DateForm />
-        <DateDay />
+        <DateNight />
+        
         
         </>
     )
 }
 
-export default DateNight;
+export default DatePlan;
 
