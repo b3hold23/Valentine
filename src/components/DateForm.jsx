b05db3fa863a34f3858f3492afa7a47899906351
unknown <rosa.angel.daniel@gmail.com>
2025-01-29 22:36:14 -0700

    completed function

--- src/components/DateForm.jsx
+++ src/components/DateForm.jsx
@@ -34,29 +34,35 @@ export default DateNight;
       const form = e.target;
       const formData = new FormData(form);
   
-      // Or you can work with it as a plain object:
-      const formJson = Object.fromEntries(formData.entries());
-      console.log(formJson);
+      // // Or you can work with it as a plain object:
+      // const formJson = Object.fromEntries(formData.entries());
+      // console.log(formJson);
+      // alert(JSON.stringify(formJson));
+      const query3 = formData.get("query3");
+      const query = formData.get("query");
+      alert(`You chose: ${query} and ${query3} on ${date.toISOString()}`);
+      
+      
     }
   
     return (
-      <form method="post" onSubmit={handleSubmit}>
+      <form  onSubmit={handleSubmit}>
         <div>
         <hr />
         <header>
           What would you like to do?
         </header>
-          <label><input type="radio" name="activityRadio" value="Movies"/> Movies </label>
-          <label><input type="radio" name="activityRadio" value="Aquarium"/> Aquarium </label>
-          <label><input type="radio" name="activityRadio" value="The Leonardo" /> The Leonardo </label>
-          <label><input type="radio" name="activityRadio" value="Zoo" /> The Zoo </label>
+          <label><input type="radio" name="query" value="Movies"/> Movies </label>
+          <label><input type="radio" name="query" value="Aquarium"/> Aquarium </label>
+          <label><input type="radio" name="query" value="The Leonardo" /> The Leonardo </label>
+          <label><input type="radio" name="query" value="Zoo" /> The Zoo </label>
         <hr />
         <header>
             Pick a restaurant:
         </header>
-          <label><input type="radio" name="myRadio" value="K-pop Fries" /> K-pop Fries </label>
-          <label><input type="radio" name="myRadio" value="Olive Garden"  /> Olive Garden </label>
-          <label><input type="radio" name="myRadio" value="Ramen" /> Ramen </label>
+          <label><input type="radio" name="query3" value="K-pop Fries" /> K-pop Fries </label>
+          <label><input type="radio" name="query3" value="Olive Garden"  /> Olive Garden </label>
+          <label><input type="radio" name="query3" value="Ramen" /> Ramen </label>
         <hr />
             <header>
             Pick a day:
--- src/pages/Date.jsx
+++ src/pages/Date.jsx
@@ -1,6 +1,4 @@
 
-
-
 import DateNight from "../components/DateForm";
 import 'react-date-picker/dist/DatePicker.css';
 import '../Calendar.css';
@@ -9,7 +7,7 @@ const DatePlan = () => {
         <>
         <img src='https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExaTlnYXVsZWV6dmY0NDQ4MTMwYW43Y2JueGN1cjQzeXdpMmttOWFreiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/d3MM4vAejYomlzGg/giphy.gif'></img>
 
-        <h1>Initial wording test</h1>
+        <h1>Page 1 Testing</h1>
         <DateNight />
         </>
     )
